4de359fd2c4357a419d1a95d314a873b8fe18aa1
qingrain <353752459@qq.com>
2021-12-16 22:36:22 +0800

    使用交互式授权码模式

---
+++ AspNetCore.Demo/IDS4-AuthorizationCode/QingRain.MvcClient.AuthorizationCode/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QingRain.MvcClient.AuthorizationCode.Models;
+using System.Diagnostics;
+using System.Linq;
+
+namespace QingRain.MvcClient.AuthorizationCode
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string? accessToken = HttpContext.GetTokenAsync("access_token").Result;
+
+            string? idToken = HttpContext.GetTokenAsync("id_token").Result;
+
+            var claimsList = from c in User.Claims select new { c.Type, c.Value };
+
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCore.Demo/IDS4-AuthorizationCode/QingRain.MvcClient.AuthorizationCode/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QingRain.MvcClient.AuthorizationCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
